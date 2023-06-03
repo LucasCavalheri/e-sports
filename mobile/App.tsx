fd84424a7253bfce14a5f67bb84482fd8f6f676b
LucasCavalheri <cav.catecumenal@gmail.com>
2023-06-03 20:44:40 -0300

    feat(mobile): configurações iniciais

---
+++ mobile/App.tsx
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello, World!</Text>
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 32,
+  },
+})
