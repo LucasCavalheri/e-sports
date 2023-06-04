188daa85c2549bdc0d51eaa9711208c38bb3ed1d
LucasCavalheri <cav.catecumenal@gmail.com>
2023-06-04 17:14:42 -0300

    feat(web): adiciona requisição à API

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from 'react'
 import logoImage from './assets/logo.svg'
 import { CreateAdBanner } from './components/CreateAdbanner'
 import { GameBanner } from './components/GameBanner'
 import './styles/main.css'
 
+interface Game {
+  id: string
+  name: string
+  bannerUrl: string
+  _count: {
+    Ad: number
+  }
+}
+
 export default function App() {
+  const [games, setGames] = useState<Game[]>([])
+
+  useEffect(() => {
+    fetch('http://localhost:3333/games')
+      .then((response) => response.json())
+      .then((data) => setGames(data))
+  }, [])
+
+  console.log(games)
+
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImage} alt="Imagem Logo" className="" />
@@ -16,7 +36,14 @@ export default function App() {
       </h1>
 
       <div className="grid grid-cols-6 gap-6 mt-16">
-        <GameBanner title='League of Legends' bannerUrl='/game-1.png' adsCount={3} />
+        {games.map((game) => (
+          <GameBanner
+            key={game.id}
+            title={game.name}
+            bannerUrl={game.bannerUrl}
+            adsCount={game._count.Ad}
+          />
+        ))}
       </div>
 
       <CreateAdBanner />
