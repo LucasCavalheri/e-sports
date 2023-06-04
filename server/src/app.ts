6d44aecb41395ba83fcc66aa34e133bb7e29998f
LucasCavalheri <cav.catecumenal@gmail.com>
2023-06-04 16:35:11 -0300

    feat(server): cria a rota /ads/:id/discord

--- server/src/app.ts
+++ server/src/app.ts
@@ -45,12 +45,37 @@ app.get('/games/:id/ads', async (req: Request, res: Response) => {
     },
   })
 
-  return res.status(200).json(ads.map((ad) => {
-    return {
-      ...ad,
-      weekDays: ad.weekDays.split(','), 
-    }
-  }))
+  return res.status(200).json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(','),
+      }
+    }),
+  )
+})
+
+app.get('/ads/:id/discord', async (req: Request, res: Response) => {
+  const adId = req.params.id
+
+  const ad = await prisma.ad.findUnique({
+    where: {
+      id: adId,
+    },
+    select: {
+      discord: true,
+    },
+  })
+
+  if (!ad) {
+    return res.status(404).json({
+      error: 'Ad not found',
+    })
+  }
+
+  return res.status(200).json({
+    discord: ad?.discord,
+  })
 })
 
 export default app
