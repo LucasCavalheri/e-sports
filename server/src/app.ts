de5c5efe92fcfecf47e7e4676c6cd33faa917b42
LucasCavalheri <cav.catecumenal@gmail.com>
2023-06-04 15:59:47 -0300

    feat(server): cria a rota /games

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,7 +1,9 @@
+import { PrismaClient } from '@prisma/client'
 import cors from 'cors'
 import 'dotenv/config'
 import express, { Request, Response } from 'express'
 
+const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -9,5 +11,17 @@ app.use(cors())
 app.get('/health-check', (_req: Request, res: Response) =>
   res.json({ status: 'ok' }),
 )
+
+app.get('/games', async (_req: Request, res: Response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: { Ad: true },
+      },
+    },
+  })
+
+  return res.status(200).json(games)
+})
 
 export default app
