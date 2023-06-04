9c7b92545cd9584ecdf672b5f0cc42c917b27a96
LucasCavalheri <cav.catecumenal@gmail.com>
2023-06-03 22:25:46 -0300

    feat(web): começa a estruturar a home page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,86 @@
+import { MagnifyingGlassPlus } from '@phosphor-icons/react'
+import logoImage from './assets/logo.svg'
+import './styles/main.css'
+
 export default function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImage} alt="Imagem Logo" className="" />
+      <h1 className="text-6xl text-white font-extrabold mt-20">
+        Seu{' '}
+        <span className="text-transparent bg-clip-text bg-gradient-to-r from-[#9572FC] via-[#43E7AD] to-[#E1D55D]">
+          duo
+        </span>{' '}
+        está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-card-game-gradient to-black/5 absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              League of Legends
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="/game-2.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-card-game-gradient to-black/5 absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Dota 2</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="/game-3.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-card-game-gradient to-black/5 absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              Counter Strike
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="/game-4.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-card-game-gradient to-black/5 absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Apex Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="/game-5.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-card-game-gradient to-black/5 absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">Fortnite</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="#" className="relative rounded-lg overflow-hidden">
+          <img src="/game-6.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-card-game-gradient to-black/5 absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white block">
+              World of Warcraft
+            </strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
+
+      <div className="pt-1 mt-8 bg-gradient-to-r from-[#9572FC] via-[#43E7AD] to-[#E2D45C] self-stretch rounded-lg overflow-hidden">
+        <div className="flex justify-between items-center px-8 py-6 h-28 bg-[#2A2634] ">
+          <div>
+            <h1 className="text-2xl text-white font-black">
+              Não encontrou seu duo?
+            </h1>
+            <span className="text-zinc-400">
+              Publique um anúncio para encontrar novos players!
+            </span>
+          </div>
+          <button className="flex justify-center items-center gap-3 bg-violet-500 py-3 px-4 hover:bg-violet-600 transition-all text-white rounded">
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ web/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'sans-serif'],
+    },
+    extend: {
+      backgroundImage: {
+        galaxy: "url('/background-galaxy.png')",
+        'card-game-gradient':
+          'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)',
+      },
+    },
+    plugins: [],
+  },
+}
